a2fd2c3b11b7569e18d926659836e46795295d4a
Scot Pfuntner <spinatose@gmail.com>
2023-11-13 18:54:33 -0600

    added product detail component and routing

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from './product';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -6,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail"
-  constructor() { }
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute, private router:Router) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle = `Product Detail: ${id}`;
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
